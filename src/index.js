8b091e594b962710d7d822d595a0a02ac145ee54
kapsteja <93643561+kapsteja@users.noreply.github.com>
2024-05-14 15:18:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createRoot} from 'react-dom'
+//import {createRoot} from 'react-dom'
 // import {Provider} from 'react-redux'
 //import store from './store/store';
 import {Provider} from 'react-redux';
